c29a17dda9ea39101f290fcffb68df92609a8c46
Mostafa <Moustafa.Achraf@hotmail.com>
2022-01-08 20:39:38 +0200

    Attempt 1 to fix cors problem

--- BackEnd/src/index.js
+++ BackEnd/src/index.js
@@ -13,12 +13,28 @@ app.use('/posters', express.static('posters'));
 app.use(express.json()); // this is going to automatically pass incoming Json to an Object so we can access it in our request handler
 app.use('/api', routes);
 
-app.use(cors()); // Use this after the variable declaration
 
 // for parsing multipart/form-data
 app.use(express.urlencoded({ extended: true }));
 app.use(upload.array());
 app.use(express.static('public'));
+
+app.use(cors()); // Use this after the variable declaration
+// app.UseCors(options => options.AllowAnyOrigin());
+// app.use(function (req, res, next) {
+//     req.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+//     req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+// });
+// const corsOptions = {
+//     origin: 'http://localhost:3000',
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200
+// }
+
+// app.use(cors(corsOptions));
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port) //The app.listen() function is used to bind and listen the connections on the specified host and port.
---
+++ FrontEnd/src/setupProxy.js
@@ -0,0 +1,14 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+// var cors = require('cors');   
+
+module.exports = function (app) {
+    // app.use(cors()); // Use this after the variable declaration
+
+    app.use(
+        '/api',
+        createProxyMiddleware({
+            target: 'http://localhost:8086',
+            changeOrigin: true,
+        })
+    );
+};
